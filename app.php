14ad51cdd59e84528e4eef08906b00761834cc8d
@iago193 <iago.silva6969@gmail.com>
2025-05-25 04:26:32 -0300

    +

--- app.php
+++ app.php
@@ -1,5 +1,46 @@
 <?php
 
     //class dashboard
+    class dashboard {
+
+        public $data_inicio;
+        public $data_fim;
+        public $numeroVendas;
+        public $totalVendas;
+
+        public function __get($atributo) {
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor) {
+            $this->$atributo = $valor;
+            return $this;
+        }
+    }
+
+    class Conexao {
+        private $host = 'localhost';
+        private $dbname = 'dashboard';
+        private $user = 'root';
+        private $pass = '';
+    }
+
+    public function conectar() {
+        try {
+
+            $conexao = new PDO(
+                "mysql:host=$this->host;dbname=$this->dbname",
+                "$this->user",
+                "$this->pass"
+            );
+
+            $conexao->('set charset utf8');
+
+            return $conexao;
+
+        } catch (PDOExeception $e) {
+            echo '<p>'.$e->getMessege().'</p>';
+        }
+    }
 
 ?>
